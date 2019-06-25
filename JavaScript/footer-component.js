d33a4e5031260ccd2d8f051ec921b1a6f501f41c
alireza <amoazeni75@gmail.com>
2019-06-25 10:30:40 +0430

    - footer component created and used

---
+++ JavaScript/footer-component.js
@@ -0,0 +1,104 @@
+
+Vue.component('footer-component', {
+    template: '<div class="m_footer_outer_div">\n' +
+        '            <div class="footer_enamd_outer">\n' +
+        '                <div class="m_header_1">\n' +
+        '                    <div class="m_footer_col_1_div">\n' +
+        '                        <span class="m_footer_col_1"></span>\n' +
+        '                        <p>مراقبت و محافظت از حساب کاربری و رمزعبور هر کاربر بر عهده کاربر است. ریحون سریعترین راه سفارش آنلاین غذا است. منوی عکس‌دار رستوران‌های اطرافتان را بر اساس مکان خود به راحتی مشاهده کنید و سفارش دهید.</p>\n' +
+        '                        <a class="forgot_link" href="">لیست رستوران‌ها</a>\n' +
+        '                    </div>\n' +
+        '                    <div class="m_footer_col_2">\n' +
+        '                        <div class="m_footer_col_2_2">\n' +
+        '                            <ul class="m_footer_col_2_ul m_footer_col_2_ul_2">\n' +
+        '                                <li class="m_footer_col_2_li">اپلیکیشن‌های موبایل</li>\n' +
+        '                                <li>\n' +
+        '                                    <div class="m_footer_col_2_outer dark">\n' +
+        '                                        <ul>\n' +
+        '                                            <li class="m_footer_col_2_outer_li m_footer_col_2_outer_li_2">\n' +
+        '                                                <a class="m_footer_col_2_outer_a footer_enamd_a_2" href="#" target="_blank" rel="nofollow noopener noreferrer"></a>\n' +
+        '                                            </li>\n' +
+        '                                            <li class="m_footer_col_2_outer_li_3 m_footer_col_2_outer_li_2">\n' +
+        '                                                <a class="m_footer_col_2_outer_a footer_enamd_a_2" href="#" target="_blank" rel="nofollow noopener noreferrer"></a>\n' +
+        '                                            </li>\n' +
+        '                                            <li class="m_footer_col_2_li_4 m_footer_col_2_outer_li_2">\n' +
+        '                                                <a class="m_footer_col_2_outer_a footer_enamd_a_2" href="#" target="_blank" rel="nofollow noopener noreferrer"></a>\n' +
+        '                                            </li>\n' +
+        '                                        </ul>\n' +
+        '                                    </div>\n' +
+        '                                </li>\n' +
+        '                            </ul>\n' +
+        '                            <ul class="m_footer_col_2_ul m_footer_col_2_ul_2">\n' +
+        '                                <li class="m_footer_col_2_li">پشتیبانی ریحون</li>\n' +
+        '                                <li><a rel="nofollow" href="#">سوالات متداول</a></li>\n' +
+        '                                <li><a href="#" rel="nofollow">تماس با پشیبانی</a></li>\n' +
+        '                                <li><a rel="nofollow" href="#">قوانین و مقررات</a></li>\n' +
+        '                            </ul>\n' +
+        '                            <ul class="m_footer_col_2_ul m_footer_col_2_ul_3">\n' +
+        '                                <li class="m_footer_col_2_li">رستوران‌ها</li>\n' +
+        '                                <li><a href="#">ثبت رستوران</a></li>\n' +
+        '                            </ul>\n' +
+        '                            <ul class="m_footer_col_2_ul m_footer_col_2_ul_4">\n' +
+        '                                <li class="m_footer_col_2_li">تماس با ریحون</li>\n' +
+        '                                <li><a rel="nofollow" href="#">درباره ریحون</a></li>\n' +
+        '                                <li><a href="#" rel="nofollow">تماس با ما</a></li>\n' +
+        '                                <li><a href="#">وبلاگ ریحون</a></li>\n' +
+        '                            </ul>\n' +
+        '                        </div>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '            </div>\n' +
+        '            <div class="footer_enamd_outer footer_enamd_outer2">\n' +
+        '                <div class="m_header_1">\n' +
+        '                    <div class="footer_enamd_container">\n' +
+        '                        <div class="footer_enamd">\n' +
+        '                            <img src="../mocks/enamad.png">\n' +
+        '                        </div>\n' +
+        '                        <div class="footer_enamd">\n' +
+        '                            <img src="../mocks/anjomanKasVaKar.png">\n' +
+        '                        </div>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '            </div>\n' +
+        '            <div class="footer_social_icons">\n' +
+        '                <div class="m_header_1">\n' +
+        '                    <ul class="footer_social_icons_ul footer_social_icons_u2">\n' +
+        '                        <li>\n' +
+        '                            <a class="footer_enamd_a_1 footer_enamd_a_2" href="" target="_blank" rel="nofollow noopener noreferrer">\n' +
+        '                            <span class="footer_social_svg_con">\n' +
+        '                                <svg class="svg1" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 409.221 409.221">\n' +
+        '                                    <g fill="none" fill-rule="evenodd"><path d="M0 0h18v18H0z"></path><path fill="#9B9B9B" d="M15.973 2.053L1.34 7.821c-.474.187-.445.875.043 1.02L5.1 9.954l1.388 4.454a.636.636 0 0 0 1.065.258l1.923-1.986 3.772 2.8a.785.785 0 0 0 1.236-.478l2.498-12.082c.123-.592-.453-1.085-1.01-.866zM14.14 4.735l-6.798 6.08a.37.37 0 0 0-.12.235l-.262 2.353c-.009.077-.115.087-.138.013l-1.076-3.51a.371.371 0 0 1 .156-.423l8.027-5.035c.184-.116.373.141.21.287z"></path>\n' +
+        '                                    </g>\n' +
+        '                                </svg>\n' +
+        '                            </span>\n' +
+        '                            </a>\n' +
+        '                        </li>\n' +
+        '                        <li>\n' +
+        '                            <a class="footer_enamd_a_1 footer_enamd_a_2" href="#" target="_blank" rel="nofollow noopener noreferrer"><span class="footer_enamd_svg4">\n' +
+        '                            <svg class="svg1" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 409.221 409.221">\n' +
+        '                                <g fill="none" fill-rule="evenodd"><path d="M0 0h18v18H0z"></path><path fill="currentColor" d="M11.999 8.985H9.836V17H6.559V8.985H5V6.168h1.559V4.345C6.559 3.04 7.17 1 9.865 1l2.427.01v2.735h-1.76c-.29 0-.696.146-.696.767V6.17h2.449l-.286 2.815z"></path></g></svg></span></a></li>\n' +
+        '                        <li>\n' +
+        '                            <a class="footer_enamd_a_1 footer_enamd_a_2" href="#" target="_blank" rel="nofollow noopener noreferrer"><span class="footer_enamd_svg3">\n' +
+        '                            <svg class="svg1" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">\n' +
+        '                                <g fill="none" fill-rule="evenodd"><path d="M0 0h18v18H0z"></path><path fill="#9B9B9B" d="M17 3.557a6.491 6.491 0 0 1-1.885.522 3.323 3.323 0 0 0 1.443-1.836 6.526 6.526 0 0 1-2.084.805A3.262 3.262 0 0 0 12.077 2c-1.813 0-3.283 1.487-3.283 3.32 0 .26.03.514.086.757a9.284 9.284 0 0 1-6.766-3.47 3.335 3.335 0 0 0-.445 1.67A3.33 3.33 0 0 0 3.13 7.04a3.231 3.231 0 0 1-1.486-.416v.042a3.313 3.313 0 0 0 2.633 3.256 3.236 3.236 0 0 1-1.483.057 3.291 3.291 0 0 0 3.067 2.306A6.539 6.539 0 0 1 1 13.66a9.209 9.209 0 0 0 5.031 1.492c6.038 0 9.34-5.06 9.34-9.448 0-.144-.003-.287-.01-.43A6.7 6.7 0 0 0 17 3.558z"></path>\n' +
+        '                                </g></svg></span></a></li>\n' +
+        '                        <li><a class="footer_enamd_a_1 footer_enamd_a_2" href="#" target="_blank" rel="nofollow noopener noreferrer"><span class="footer_enamd_svg2">\n' +
+        '                            <svg class="svg1" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 409.221 409.221">\n' +
+        '                                <g fill="#9B9B9B"><path d="M12.52 1H5.392A4.396 4.396 0 0 0 1 5.391v7.13a4.396 4.396 0 0 0 4.391 4.39h7.13a4.396 4.396 0 0 0 4.39-4.39V5.39A4.396 4.396 0 0 0 12.522 1zm2.98 11.52a2.983 2.983 0 0 1-2.98 2.98H5.392a2.983 2.983 0 0 1-2.98-2.98V5.392a2.983 2.983 0 0 1 2.98-2.98h7.13a2.983 2.983 0 0 1 2.979 2.98v7.13z"></path><path d="M8.956 4.856c-2.26 0-4.1 1.84-4.1 4.1s1.84 4.1 4.1 4.1 4.1-1.84 4.1-4.1-1.84-4.1-4.1-4.1zm0 6.788a2.691 2.691 0 0 1-2.688-2.688 2.691 2.691 0 0 1 2.688-2.688 2.691 2.691 0 0 1 2.688 2.688 2.691 2.691 0 0 1-2.688 2.688zM13.228 3.659a1.04 1.04 0 0 0-1.035 1.035c0 .272.11.54.304.732.192.192.459.304.73.304.274 0 .54-.112.733-.304a1.04 1.04 0 0 0 0-1.464 1.04 1.04 0 0 0-.732-.303z"></path>\n' +
+        '                                </g></svg></span></a></li>\n' +
+        '                        <li><a class="footer_enamd_a_1"        href="#" target="_blank" rel="publisher noopener noreferrer nofollow" ><span class="footer_enamd_container_svg">\n' +
+        '                            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 458.246 458.246" xml:space="preserve">\n' +
+        '                                <g><path class="svg1" d="M160.777,259.368h71.594c-12.567,35.53-46.603,61.004-86.45,60.71 c-48.349-0.357-88.327-39.035-90.204-87.349c-2.012-51.789,39.537-94.563,90.887-94.563c23.479,0,44.905,8.946,61.058,23.605 c3.826,3.473,9.65,3.495,13.413-0.047l26.296-24.749c4.112-3.871,4.127-10.408,0.027-14.292 c-25.617-24.269-59.981-39.396-97.876-40.136C68.696,80.969,0.567,147.238,0.004,228.078 c-0.568,81.447,65.285,147.649,146.6,147.649c78.199,0,142.081-61.229,146.36-138.358c0.114-0.967,0.189-33.648,0.189-33.648 H160.777c-5.426,0-9.824,4.398-9.824,9.824v35.999C150.953,254.97,155.352,259.368,160.777,259.368z"></path><path class="svg1" d="M414.464,206.99v-35.173c0-4.755-3.854-8.609-8.609-8.609h-29.604c-4.755,0-8.609,3.854-8.609,8.609 v35.173h-35.173c-4.755,0-8.609,3.854-8.609,8.609v29.604c0,4.755,3.854,8.609,8.609,8.609h35.173v35.173 c0,4.755,3.854,8.609,8.609,8.609h29.604c4.755,0,8.609-3.854,8.609-8.609v-35.173h35.173c4.755,0,8.609-3.854,8.609-8.609v-29.604 c0-4.755-3.854-8.609-8.609-8.609L414.464,206.99L414.464,206.99z"></path>\n' +
+        '                                </g></svg></span></a></li>\n' +
+        '                    </ul>\n' +
+        '                    <p>\n' +
+        '                        © 2017,\n' +
+        '                        <a href="">S.Alireza Moazeni 9423110</a>\n' +
+        '                        , All Rights Reserved.\n' +
+        '                    </p>\n' +
+        '                </div>\n' +
+        '            </div>\n' +
+        '    </div>'
+})
+
+new Vue({ el: '#footer-c' })
