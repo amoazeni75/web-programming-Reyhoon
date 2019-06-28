c969c9bdb269c55f037f5cab9b3ca935baad98ff
S.A.M.P <amoazeni75@gmail.com>
2019-06-28 17:13:52 +0430

    SoftDelete and Global Scope added

--- app/Category.php
+++ app/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Category extends Model
 {
 	use SoftDeletes;
-    
+    protected $dates = ['deleted_at'];
     protected $fillable = [
     	'name',
     	'description',
