7266dafe4e5be2ff2e818be69413aeaa01950c64
S.A.M.P <amoazeni75@gmail.com>
2019-06-28 10:35:45 +0430

    bug fixed

--- app/Seller.php
+++ app/Seller.php
@@ -1,11 +1,12 @@
 <?php
-
 namespace App;
 use App\Product;
 
 class Seller extends User
 {
-    public function products(){
-    	return hasMany(Product::class);
+	
+    public function products()
+    {
+    	return $this->hasMany(Product::class);
     }
 }
