0df43fbc64334de2ac2a3231ffd01eec628e6a4a
S.A.M.P <amoazeni75@gmail.com>
2019-06-29 10:24:40 +0430

    implementation of product's controllers

---
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\User;
+use App\Seller;
+use App\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class SellerProductController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Seller $seller)
+    {
+        $products = $seller->products;
+        return $this->showAll($products);
+    }
+
+
+    // we implement store a product for a specific seller here because
+    // in future we can control access
+    // we pass instance of user insead of seller in order to provide ability
+    // of registering a product for a person who has not any product yet
+    **
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, User $seller)
+    {
+        $rules = [
+            'name' => 'required',
+            'description' => 'required',
+            'quantity' => 'required|integer|min:1',
+            'image' => 'required|image',
+        ];
+        $this->validate($request, $rules);
+        $data = $request->all();
+        $data['status'] = Pr oduct::UNAVAILABLE_PRODUCT;
+        $data['image'] = $request->image->store('');
+        $data['seller_id'] = $seller->id;
+        $product = Product::create($data);
+        return $this->showOne($product);
+    }
+
+       /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Seller $seller, Product $product)
+    {
+        $rules = [
+            'quantity' => 'integer|min:1',
+            'status' => 'in:' . Product::AVAILABLE_PRODUCT . ',' . Product::UNAVAILABLE_PRODUCT,
+            'image' => 'image',
+        ];
+        $this->validate($request, $rules);
+        $this->checkSeller($seller, $product);
+        $product->fill($request->only([
+            'name',
+            'description',
+            'quantity',
+        ]));
+        if ($request->has('status')) {
+            $product->status = $request->status;
+            if ($product->isAvailable() && $product->categories()->count() == 0) {
+                return $this->errorResponse('An active product must have at least one category', 409);
+            }
+        }
+        if ($request->hasFile('image')) {
+            Storage::delete($product->image);
+            $product->image = $request->image->store('');
+        }
+        if ($product->isClean()) {
+            return $this->errorResponse('You need to specify a different value to update', 422);
+        }
+        $product->save();
+        return $this->showOne($product);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -38,8 +38,14 @@ Route::resource('categories.transactions','Category\CategoryTransactionControlle
 Route::resource('categories.buyers','Category\CategoryBuyerController', ['only' => ['index']]);
 
 Route::resource('products','Product\ProductController', ['only' => ['index', 'show']]);
+Route::resource('products.transactions','Product\ProductTransactionController', ['only' => ['index', 'show']]);
+
 
 Route::resource('sellers','Seller\SellerController', ['only' => ['index', 'show']]);
+Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only' => ['index']]);
+Route::resource('sellers.categories', 'Seller\SellerCategoryController', ['only' => ['index']]);
+Route::resource('sellers.buyers', 'Seller\SellerBuyerController', ['only' => ['index']]);
+Route::resource('sellers.products', 'Seller\SellerProductController', ['except' => ['create', 'show', 'edit']]);
 
 Route::resource('transactions','Transaction\TransactionController', ['only' => ['index', 'show']]);
 
