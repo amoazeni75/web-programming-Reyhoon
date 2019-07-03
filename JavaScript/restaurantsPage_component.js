26b77e3f2466c52aaf4f6c205a8c0035240ac2d3
S.A.M.P <amoazeni75@gmail.com>
2019-07-03 08:47:20 +0430

    new face for category

--- JavaScript/restaurantsPage_component.js
+++ JavaScript/restaurantsPage_component.js
@@ -5,10 +5,26 @@ new Vue({
         area: "",
         activeRestaurants: [],
         deactivateRestaurants: [],
-        categories: ['ساندویچ', 'برگر', 'پاستا', 'کباب', 'خورشت', 'غذای ایرانی', 'خوراک'],
-
+        //categories: ['ساندویچ', 'برگر', 'پاستا', 'کباب', 'خورشت', 'غذای ایرانی', 'خوراک'],
+        categories: [],
+        dictionaryArr: [
+            {key: "sandwich", value: 'ساندویچ'},
+            {key: "berger", value: 'برگر'},
+            {key: "khoresht", value: 'خورشت'},
+            {key: "kabab", value: 'کباب'},
+            {key: "pasta", value: 'پاستا'},
+            {key: "irani", value: 'غذای ایرانی'},
+            {key: "khourak", value: 'خوراک'},
+        ],
+        processCategory: true,
     },
     methods: {
+        translateEnglishToPersian(word) {
+            for (let i = 0; i < this.dictionaryArr.length; i++) {
+                if (this.dictionaryArr[i].key == word)
+                    return this.dictionaryArr[i].value
+            }
+        },
         parseQueryPart() {
             page_url = decodeURIComponent(window.location.search);
             page_url = page_url.slice(1)
@@ -16,11 +32,23 @@ new Vue({
             this.city = page_url[0].slice(5);
             this.area = page_url[1].slice(5);
         },
+        prepareCategories(categories_server) {
+            for (cat in categories_server) {
+                this.categories.push(
+                    {
+                        "name": this.translateEnglishToPersian(cat),
+                        "quantity": categories_server[cat]
+                    });
+            }
+        },
         handleListOfRestaurants(xhttp) {
             jsonDOM = JSON.parse(xhttp.responseText);
             restaurants_count = jsonDOM.length - 1;
-            document.getElementById("text_count_rest").innerHTML = restaurants_count + " رستوران امکان سرویس دهی به" ;
+            document.getElementById("text_count_rest").innerHTML = restaurants_count + " رستوران امکان سرویس دهی به";
             document.getElementById("text_address_rest").innerHTML = this.city + "،" + this.area;
+            if (this.processCategory) {
+                this.prepareCategories(jsonDOM[jsonDOM.length - 1]);
+            }
         },
         getDataFromServer(url, handleFunction, method, queryPart) {
             let xhttp = new XMLHttpRequest();
