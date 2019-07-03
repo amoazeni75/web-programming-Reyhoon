fbc6762de90117a4ba69d461aa5ef9883f226dde
S.A.M.P <amoazeni75@gmail.com>
2019-07-04 00:26:10 +0430

    details of restaurant in single page added
    - restaurant name
    - restaurant rate
    - restaurant comments
    added

--- JavaScript/singleRestaurant_component.js
+++ JavaScript/singleRestaurant_component.js
@@ -6,6 +6,8 @@ new Vue({
         restaurant_object: "",
         allFoods : [],
         categories : [],
+        comments : [],
+        avg_ratings : [],
         dictionaryArr: [
             {key: "sandwich", value: 'ساندویچ'},
             {key: "berger", value: 'برگر'},
@@ -14,11 +16,15 @@ new Vue({
             {key: "pasta", value: 'پاستا'},
             {key: "irani", value: 'غذای ایرانی'},
             {key: "khourak", value: 'خوراک'},
+            {key: "quality", value: 'کیفیت غذا'},
+            {key: "packing", value: 'کیفیت بسته بندی'},
+            {key: "deliveryRate", value: 'سرعت ارسال پیک'},
         ],
     },
 
     methods: {
         translateEnglishToPersian(word) {
+            console.log(word);
             for (let i = 0; i < this.dictionaryArr.length; i++) {
                 if (this.dictionaryArr[i].key == word)
                     return this.dictionaryArr[i].value
@@ -32,9 +38,9 @@ new Vue({
         },
         prepareCategories(categories_server) {
             for (cat in categories_server) {
-                this.categories.push(
+               this.categories.push(
                     {
-                        "name": this.translateEnglishToPersian(categories_server[cat]),
+                        "name": this.translateEnglishToPersian(categories_server[cat].name),
                         "foods" : [],
                     });
             }
@@ -53,6 +59,13 @@ new Vue({
             this.prepareCategories(jsonDOM.categories);
             this.prepareFood(jsonDOM.foods)
             this.restaurant_object = jsonDOM;
+        },
+        handleListOfComments(xhttp){
+            jsonDOM = JSON.parse(xhttp.responseText);
+            this.avg_ratings = jsonDOM.pop();
+            for (comment in jsonDOM){
+               this.comments.push(jsonDOM[comment]);
+            }
         },
         getDataFromServer(url, handleFunction, method, queryPart) {
             let xhttp = new XMLHttpRequest();
@@ -76,5 +89,11 @@ new Vue({
             this.handleListOfFoods,
             "GET",
             '');
+        this.getDataFromServer(
+            "http://restfulapi.test/api/restaurants/" + this.restaurant_id+"/" + "comments",
+            this.handleListOfComments,
+            "GET",
+            ''
+        );
     }
 })
